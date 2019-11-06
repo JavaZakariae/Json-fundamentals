52b5e59d3c4ac3c17348d22726a11a0ec50abba0
bouali zakariae <bouali_zakariae@hotmail.fr>
2019-11-06 20:41:29 +0100

    manual creation of json object.

--- src/main/java/JsonRunner.java
+++ src/main/java/JsonRunner.java
@@ -1,9 +1,38 @@
+import commun.Job;
+import commun.Loan;
+import commun.LoanDetails;
+
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  *
  */
 public class JsonRunner {
+
+    static Loan loan;
+    static {
+        Job job1 = new Job("delhaize", 500, 5);
+        System.out.println("job1 = " + job1);
+
+        Job job2 = new Job("Mainsys", 2000, 5);
+        System.out.println("job2 = " + job2);
+
+
+        LoanDetails loanDetails = new LoanDetails(2500, LocalDate.of(2019,04,20),
+                LocalDate.of(2029,04,20));
+        System.out.println("loanDetails = " + loanDetails);
+
+        loan = new Loan("Bouali Zakariae", "buying House", loanDetails, List.of(job1, job2));
+        System.out.println("loan = " + loan);
+    }
+
     public static void main( String[] args ) {
 
+    }
+
+    static Loan getLoan(){
+        return loan;
     }
 
 
--- src/main/java/ManJsonParsing.java
+++ src/main/java/ManJsonParsing.java
@@ -2,27 +2,44 @@ import commun.Job;
 import commun.Loan;
 import commun.LoanDetails;
 
+import java.lang.ref.SoftReference;
 import java.time.LocalDate;
 import java.util.List;
 
 /**
  * Manual Json Parsing
+ *
+ *  Producing JSON Demo
  */
 public class ManJsonParsing {
 
     public static void main( String[] args ) {
-        Job job1 = new Job("delhaize", 500, 5);
-        System.out.println("job1 = " + job1);
-
-        Job job2 = new Job("Mainsys", 2000, 5);
-        System.out.println("job2 = " + job2);
-
-
-        LoanDetails loanDetails = new LoanDetails(2500, LocalDate.of(2019,04,20),
-                                            LocalDate.of(2029,04,20));
-        System.out.println("loanDetails = " + loanDetails);
-
-        Loan loan = new Loan("Bouali Zakariae", "buying House", loanDetails, List.of(job1, job2));
-        System.out.println("loan = " + loan);
+        toJson(JsonRunner.getLoan());
     }
+
+    /**
+     * manual operation to create a json object representation
+     * As we can see, it is hard to create manually an object
+     * json from a java class object
+     *
+     * @param loan
+     */
+    private static void toJson(Loan loan){
+        System.out.println("ToString");
+        System.out.println(loan);
+        System.out.println("jsonPresentation");
+
+        final String json = String.format("{\n" +
+                        "\"name\":\"%s\",\n" +
+                        "\"purposeofloan\":\"%s\",\n" +
+                        "\"details\":\"%s\",\n" +
+                        "\"jobs\":\"%s\",\n" +
+                        "}",
+                loan.getName(),
+                loan.getPurposeOfLoan(),
+                loan.getLoanDetails(),
+                loan.getJobs());
+        System.out.println("loan json object = " + json);
+    }
+
 }
