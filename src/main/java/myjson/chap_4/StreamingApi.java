5f797b2340c58b0a575ddb6fe30bd6427cade066
bouali zakariae <bouali_zakariae@hotmail.fr>
2019-11-11 15:17:48 +0100

    Demo: json streaming Api example printing the full document

---
+++ src/main/java/myjson/chap_4/StreamingApi.java
@@ -0,0 +1,31 @@
+package myjson.chap_4;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * the streaming Api is more powerful in term of time consuming than the binding api.
+ * Binding Api create memory tree to represent the corresponding json document.
+ *
+ */
+public class StreamingApi {
+
+    private static File loanFile = new File("src/main/resources/loan.json");
+
+    public static void main( String[] args ) throws IOException {
+
+        JsonFactory jsonFactory = new JsonFactory();
+        try (JsonParser jsonParser = jsonFactory.createParser(loanFile)){
+            JsonToken jsonToken;
+            while ((jsonToken=jsonParser.nextToken()) != null )
+            {
+                System.out.print("jsonToken = " + jsonToken);
+                System.out.println("    jsonParser = " + jsonParser.getText());
+            }
+        }
+    }
+}
